0884607df4ec47af63ee609c2fe3c1d29b740858
Igor Malyarov <igor1309@gmail.com>
2020-12-23 22:16:21 +0300

    Initial Commit

---
+++ TextViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextViewApp
+//
+//  Created by Igor Malyarov on 23.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextViewApp/TextViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextViewAppApp.swift
+//  TextViewApp
+//
+//  Created by Igor Malyarov on 23.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TextViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
